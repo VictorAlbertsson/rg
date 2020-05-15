fb618bf63d6828f540f086d8adef9bf3f90fd68f
Victor Albertsson <victor.albertsson@live.se>
2020-05-15 09:03:45 +0200

    Updated README to include information desired by my teacher.

--- src/events.rs
+++ src/events.rs
@@ -1,16 +1,5 @@
 pub type Event = dyn Fn() -> bool;
 
-pub struct LayerStack {
-    pub layers: Vec<Layer>,
-}
-
-impl LayerStack {
-    pub fn push_layer() {}
-    pub fn push_overlay() {}
-    pub fn pop_layer() {}
-    pub fn pop_overlay() {}
-}
-
 pub struct Layer {
     pub name: String,
     pub events: Vec<Box<Event>>,
@@ -40,4 +29,15 @@ impl Layer {
 	    })
 	    .collect()
     }
+}
+
+pub struct LayerStack {
+    pub layers: Vec<Layer>,
+}
+
+impl LayerStack {
+    pub fn push_layer() {}
+    pub fn push_overlay() {}
+    pub fn pop_layer() {}
+    pub fn pop_overlay() {}
 }
